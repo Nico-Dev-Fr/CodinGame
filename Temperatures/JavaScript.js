7fd27f1145b952af9b91d81fa28e953b12c2974a
admin <admin@vps-7a291c78.vps.ovh.net>
2021-06-10 14:22:42 +0000

    Refactoring Temperature JS

--- Temperatures/JavaScript.js
+++ Temperatures/JavaScript.js
@@ -3,16 +3,8 @@ var inputs = readline().split(' ');
 var closest = 0;
 for (let i = 0; i < n; i++) {
     const t = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
-    if(closest === 0){
-        closest = t;
-    }else if(t > 0 && t <= Math.abs(closest) ){
-        closest = t
-    }else if(t < 0 && -t < Math.abs(closest) ){
+    if(closest === 0 || (t > 0 && t <= Math.abs(closest)) || (t < 0 && -t < Math.abs(closest) )){
         closest = t;
     }
 }
-
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
 console.log(closest);
